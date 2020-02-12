e3845082cf25af2ca0707c52eb3398423e17b927
Ben Larson <benlarson_fullstack@yahoo.com>
2020-02-12 11:08:07 -0600

    message

---
+++ ClassRoster/src/main/java/com/bl/classroster/App.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bl.classroster;
+
+import com.bl.classroster.controller.*;
+import com.bl.classroster.dao.*;
+import com.bl.classroster.service.*;
+import com.bl.classroster.ui.*;
+
+/**
+ *
+ * @author benth
+ */
+public class App {
+    public static void main(String[] args) {
+        // Instantiate the UserIO implementation
+        UserIO myIo = new UserIOConsoleImpl();
+        // Instantiate the View and wire the UserIO implementation into it
+        ClassRosterView myView = new ClassRosterView(myIo);
+        // Instantiate the DAO
+        ClassRosterDao myDao = new ClassRosterDaoFileImpl();
+        // Instantiate the Audit DAO
+        ClassRosterAuditDao myAuditDao = new ClassRosterAuditDaoFileImpl();
+        // Instantiate the Service Layer and wire the DAO and Audit DAO into it
+        ClassRosterServiceLayer myService = new ClassRosterServiceLayerImpl(myDao, myAuditDao);
+        // Instantiate the Controller and wire the Service Layer into it
+        ClassRosterController controller = new ClassRosterController(myService, myView);
+        // Kick off the Controller
+        controller.run();
+    }
+}
